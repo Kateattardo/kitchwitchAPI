d6e8568041df35571e39a10a3b8c6cc9483f013c
Kateattardo <youremail@domain.com>
2023-10-17 14:38:37 -0400

    deploy

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,21 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const commentSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
+const commentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    recipeId: {
+      type: Number,
+      ref: "Recipe",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
   },
-  recipeId: {
-    type: Number,
-    ref: 'Recipe',
-    required: true,
-  },
-  text: {
-    type: String,
-    required: true,
-  },
+  { timestamps: true }
+);
 
-}, { timestamps: true });
-
-module.exports = mongoose.model('Comment', commentSchema);
+module.exports = mongoose.model("Comment", commentSchema);
